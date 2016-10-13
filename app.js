61de395a713e24f0ca66a58d537fb4a7b856f95b
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-10-14 08:48:33 +0900

    Use static html file.

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-var http = require('http');
-var port = process.env.PORT || 3000;
+var connect = require('connect');
+var serveStatic = require('serve-static');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('\
-<!DOCTYPE html>\n\
-<html>\n\
-  <head>\n\
-    <title>Hello</title>\n\
-  </head>\n\
-  <body>\n\
-    <h1>Hello</h1> World!\n\
-  </body>\n\
-</html>\
-  ');
-}).listen(port);
+var app = connect();
+app.use(serveStatic('static'));
+app.listen(3000);
 
-console.log('Server running at http://127.0.0.1:' + port + '/');
+console.log('Server running at http://127.0.0.1:3000/');
