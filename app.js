b1b9ef2e6fdc2b35cacfcf31d92b35002fd4a339
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-10-14 08:46:36 +0900

    Write HTML instead of plain text.

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 var http = require('http');
 var port = process.env.PORT || 3000;
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('\
+<!DOCTYPE html>\n\
+<html>\n\
+  <head>\n\
+    <title>Hello</title>\n\
+  </head>\n\
+  <body>\n\
+    <h1>Hello</h1> World!\n\
+  </body>\n\
+</html>\
+  ');
 }).listen(port);
+
 console.log('Server running at http://127.0.0.1:' + port + '/');
