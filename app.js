7078354ea7da06c998bf9d539c355405925bc89a
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-10-14 09:17:24 +0900

    Refactor maze generator.

--- app.js
+++ app.js
@@ -12,40 +12,51 @@ function createPage(title, body) {
 </html>';
 }
 
+function createGaze(directions) {
+  var body;
+  if (directions) {
+    body = 'どの方向に進む？<ul>';
+    if (directions.up) {
+      body += '<li><a href="' + directions.up + '">↑</a>'
+    }
+    if (directions.right) {
+      body += '<li><a href="' + directions.right + '">→</a>'
+    }
+    if (directions.down) {
+      body += '<li><a href="' + directions.down + '">↓</a>'
+    }
+    if (directions.left) {
+      body += '<li><a href="' + directions.left + '">←</a>'
+    }
+    body += '</ul>';
+  } else {
+    body = 'ゴール！<br /><a href="index.html">トップページへ</a>'
+  }
+  return createPage('迷路ゲーム', body);
+}
+
 var index = createPage('My Web Apps',
     '<a href="maze1.html">迷路ゲーム</a>');
-var maze1 = createPage('迷路ゲーム',
-    'どの方向に進む？\n\
-    <ul>\n\
-      <li><a href="maze2.html">↓</a>\n\
-    </ul>');
-var maze2 = createPage('迷路ゲーム',
-    'どの方向に進む？\n\
-    <ul>\n\
-      <li><a href="maze1.html">↑</a>\n\
-      <li><a href="maze3.html">→</a>\n\
-      <li><a href="maze4.html">↓</a>\n\
-      <li><a href="maze5.html">←</a>\n\
-    </ul>');
-var maze3 = createPage('迷路ゲーム',
-    'どの方向に進む？\n\
-    <ul>\n\
-      <li><a href="maze6.html">→</a>\n\
-      <li><a href="maze2.html">←</a>\n\
-    </ul>');
-var maze4 = createPage('迷路ゲーム',
-    'どの方向に進む？\n\
-    <ul>\n\
-      <li><a href="maze2.html">↑</a>\n\
-    </ul>');
-var maze5 = createPage('迷路ゲーム',
-    'どの方向に進む？\n\
-    <ul>\n\
-      <li><a href="maze2.html">→</a>\n\
-    </ul>');
-var maze6 = createPage('迷路ゲーム',
-    'ゴール！<br />\n\
-    <a href="index.html">トップページへ</a>');
+var maze1 = createGaze({
+  down: 'maze2.html'
+});
+var maze2 = createGaze({
+  up: 'maze1.html',
+  right: 'maze3.html',
+  down: 'maze4.html',
+  left: 'maze5.html'
+});
+var maze3 = createGaze({
+  right: 'maze6.html',
+  left: 'maze2.html'
+});
+var maze4 = createGaze({
+  up: 'maze2.html',
+});
+var maze5 = createGaze({
+  right: 'maze2.html',
+});
+var maze6 = createGaze();
 
 var http = require('http');
 var port = process.env.PORT || 3000;
